7bb8c109184a05d5914e2c5fec51048b8f43da0e
joyshin <socrashin@gmail.com>
2012-04-23 23:49:05 +0900

    2012 04 23

--- src/net/skcomms/lucene/joyshin/SearchServlet.java
+++ src/net/skcomms/lucene/joyshin/SearchServlet.java
@@ -1,43 +1,33 @@
 package net.skcomms.lucene.joyshin;
 
-import java.io.IOException;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.DateTools;
-import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.util.Version;
+/*public class SearchServlet extends HttpServlet {
+ // import javax.servlet.http.*;
+ // import javax.servlet.*;
 
-public class SearchServlet extends HttpServlet {
-  // import javax.servlet.http.*;
-  // import javax.servlet.*;
+ private IndexSearcher searcher;
 
-  private IndexSearcher searcher;
+ protected void doGet(HttpServletRequest request,
+ HttpServletResponse response)
+ throws ServletException, IOException {
 
-  protected void doGet(HttpServletRequest request,
-      HttpServletResponse response)
-      throws ServletException, IOException {
+ QueryParser parser = new NumericDateRangeQueryParser(Version.LUCENE_35,
+ "contents",
+ new StandardAnalyzer(Version.LUCENE_35));
 
-    QueryParser parser = new NumericDateRangeQueryParser(Version.LUCENE_35,
-        "contents",
-        new StandardAnalyzer(Version.LUCENE_35));
+ parser.setLocale(request.getLocale());
+ parser.setDateResolution(DateTools.Resolution.DAY);
 
-    parser.setLocale(request.getLocale());
-    parser.setDateResolution(DateTools.Resolution.DAY);
+ Query query = null;
+ try {
+ query = parser.parse(request.getParameter("q"));
+ } catch (ParseException e) {
+ e.printStackTrace(System.err); // 1
+ }
 
-    Query query = null;
-    try {
-      query = parser.parse(request.getParameter("q"));
-    } catch (ParseException e) {
-      e.printStackTrace(System.err); // 1
-    }
+ TopDocs docs = this.searcher.search(query, 10); // 2
+ }
 
-    TopDocs docs = this.searcher.search(query, 10); // 2
-  }
-  /*
-   * 1 Handle exception 2 Perfom search and render results
-   */
-}
+ * 1 Handle exception 2 Perfom search and render results
+
+ }*/
--- src/net/skcomms/lucene/joyshin/test/BookLinkCollectorTest.java
+++ src/net/skcomms/lucene/joyshin/test/BookLinkCollectorTest.java
@@ -10,21 +10,25 @@ import net.skcomms.lucene.joyshin.TestUtil;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.store.Directory;
 import org.junit.Test;
 
 public class BookLinkCollectorTest {
+  public static final String INDEX_DIRECTORY = "indexDirectory";
 
   @Test
   public void testCollecting() throws Exception {
     TermQuery query = new TermQuery(new Term("contents", "junit"));
-    IndexSearcher searcher = new IndexSearcher(TestUtil.getBookIndexDirectory());
+    // IndexSearcher searcher = new
+    // IndexSearcher(TestUtil.getBookIndexDirectory());
+    Directory directory = TestUtil.getBookIndexDirectory();
+    IndexSearcher searcher = new IndexSearcher(directory);
 
-    BookLinkCollector collector = new BookLinkCollector(searcher);
+    BookLinkCollector collector = new BookLinkCollector();
     searcher.search(query, collector);
 
     Map linkMap = collector.getLinks();
-    assertEquals("java development with ant", linkMap.get("http://www.manning.com/antboot"));
+    assertEquals("java development with ant", linkMap.get("http://www.manning.com/antbook"));
     searcher.close();
   }
-
 }
